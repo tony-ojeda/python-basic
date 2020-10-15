b19655645553ab849ce21ba6c93640edd04e46c0
Tony Ojeda <tojeda96@gmail.com>
2020-10-15 13:40:44 -0500

    feature: sum test

--- super_calculadora/ut_supercalculadora.py
+++ super_calculadora/ut_supercalculadora.py
@@ -3,13 +3,21 @@ import supercalculadora
 
 class TestsSupercalculadora(unittest.TestCase):
     
+    def setUp(self):
+        self.calc = supercalculadora.Supercalculadora()
+
+    def tearDown(self):
+        pass
+    
     def test_sumar_2_y_2(self):
-        calc = supercalculadora.Supercalculadora()
-        self.assertEqual(4, calc.sumar(2,2))
+        self.assertEqual(4, self.calc.sumar(2,2))
     
     def test_sumar_5_y_7(self):
-        calc = supercalculadora.Supercalculadora()
-        self.assertEqual(12, calc.sumar(5,7))
+        self.assertEqual(12, self.calc.sumar(5,7))
+
+    def test_sumar_propiedad_conmutativa(self):
+        self.assertEqual(self.calc.sumar(5,7),
+        self.calc.sumar(7,5))
 
 if __name__ == "__main__":
     unittest.main()
